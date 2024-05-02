b46f208955263f090d3545ae60ca64f7c335381f
lbielicki <lbielicki24@cmc.edu>
2024-05-01 17:05:31 -0700

    normalized_batch

--- services/pg_normalized_batch/schema-indexes.sql
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -1,5 +1,9 @@
-create index on tweet_tags(id_tweets, tag);
 create index on tweet_tags(tag, id_tweets);
+
+create index on tweet_tags(id_tweets, tag);
+
 create index on tweets(id_tweets, lang);
+create index on tweets(id_tweets);
 create index on tweets(lang);
-create index tweets_text on tweets using GIN(to_tsvector('english', text)) where lang = 'en';
+
+create index on tweets using gin(to_tsvector('english', text)) where lang = 'en';
